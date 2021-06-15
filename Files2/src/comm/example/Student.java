6b6e90fe98881058a4ba6bc363554e1b6f409670
001Z84744 <001Z84744@LAPTOP-RV76KLVD>
2021-06-15 17:57:19 +0530

    added lombok

---
+++ Files2/src/comm/example/Student.java
@@ -0,0 +1,36 @@
+package comm.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student implements Comparable{
+	
+	private String studentId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private double gpa;
+	
+
+	
+	@Override
+	public int compareTo(Object o) {
+		if(this.gpa>((Student)o).gpa)
+			return -1;
+		if(this.gpa<((Student)o).gpa)
+			return 1;
+		else
+			return 0;
+	}
+	
+	
+
+}
