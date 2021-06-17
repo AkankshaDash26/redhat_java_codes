54365c71025b211b04387edf4f607ccc159b0221
001Z84744 <001Z84744@LAPTOP-RV76KLVD>
2021-06-17 10:15:49 +0530

    added PrintThread class to the Threads project.

--- Threads/src/comm/example/app/App.java
+++ Threads/src/comm/example/app/App.java
@@ -1,11 +1,14 @@
 package comm.example.app;
 
+
+import comm.example.PrintThread;
 import comm.example.ThreadTest;
 
 public class App {
 	
 	public static void main(String[] args)
 	{
+	 /*
 		ThreadTest test=new ThreadTest();
 		Thread t=new Thread(test);
 		System.out.println(t.getName());
@@ -13,6 +16,11 @@ public class App {
 		Thread t1=new Thread(test);
 		System.out.println(t1.getName());
 		t1.start();
+	 */
+		PrintThread t1=new PrintThread("A");
+		t1.start();
+		PrintThread t2=new PrintThread("B");
+		t2.start();
 	}
 
 }
