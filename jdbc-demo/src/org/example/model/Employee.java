76c576d498848009fea434379d91462cc19dedb0
001Z84744 <001Z84744@LAPTOP-RV76KLVD>
2021-06-21 18:11:33 +0530

    jdbc updated

--- jdbc-demo/src/org/example/model/Employee.java
+++ jdbc-demo/src/org/example/model/Employee.java
@@ -1,54 +1,20 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Employee {
-	
 	private Integer id;
 	private String firstName;
 	private String lastName;
 	private String email;
-	
-	public Employee() {
-		super();
-	}
-	
-	public Employee(Integer id, String firstName, String lastName, String email) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
 
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
-	
-    
 }
