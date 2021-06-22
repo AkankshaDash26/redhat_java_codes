bd89acfe08a4da2cf923145a81f9cf5ea784f2d0
001Z84744 <001Z84744@LAPTOP-RV76KLVD>
2021-06-22 22:55:18 +0530

    hibernate1

---
+++ hibernate-demo/src/main/java/org/example/hibernate_demo/App.java
@@ -0,0 +1,47 @@
+package org.example.hibernate_demo;
+
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.example.hibernate_demo.model.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	static Logger logger = Logger.getLogger("org.example.demo.App.class");
+
+	public static void main(String[] args) {
+		PropertyConfigurator.configure("log4j.properties");
+		try {
+			Properties prop = new Properties();
+			prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/hr");
+
+			// prop.setProperty("dialect", "org.hibernate.dialect.PostgresSQL");
+
+			prop.setProperty("hibernate.connection.username", "root");
+			prop.setProperty("hibernate.connection.password", "root");
+			// prop.setProperty("hibernate.connection.driver_class",
+			// "org.postgresql.Driver");
+
+			// SessionFactory factory=new
+			// Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class).buildSessionFactory();
+			logger.info("session factory created");
+			SessionFactory factory = new Configuration().addProperties(prop).addAnnotatedClass(Employee.class)
+					.buildSessionFactory();
+			Session session = factory.openSession();
+			logger.info("session open");
+			session.getTransaction().begin();
+			session.save(new Employee(new Random().nextInt(100000), "Daniel", "Mark", "daniel@email.com"));
+			session.getTransaction().commit();
+			System.out.println("one item saved");
+			logger.info("done");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
